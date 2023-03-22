437fe2f5bd9d476554c5b52e59f1d3188d0707ec
Paul Buser <buser.paul@gmail.com>
2023-03-22 12:13:37 -0400

    Make WebSockets work.

--- audience-app/App.js
+++ audience-app/App.js
@@ -1,25 +1,52 @@
+import { useState, useRef, useEffect } from 'react';
 import { StyleSheet, View, Text, Pressable } from 'react-native';
 
-export default function App() {
+
+const WS_BACKEND = "ws://192.168.0.69:8080";
+
+const App = () => {
+
+  const [data, setData] = useState({
+    choice1: "Choice 1",
+    choice2: "Choice 2",
+    question: "Question"
+  });
+
+  const ws = useRef(null);
+
+
+  useEffect(() => {
+    ws.current = new WebSocket(WS_BACKEND);
+    ws.current.onmessage = (message) => {
+      console.log(message)
+      setData(JSON.parse(message.data));
+    };
+    return () => ws.current.close();
+  }, []);
+
+  const sendMessage = (message) => {
+    alert('sending ' + message + ' to server');
+    ws.current.send(message);
+  }
+
   return (
     <View
       style={[
         styles.container,
         {
-          // Try setting `flexDirection` to `"row"`.
           flexDirection: 'column',
         },
       ]}>
-      <Pressable style={{ flex: 1, backgroundColor: 'red' }} >
-        <Text>Voting App</Text>
-      </Pressable>
+      <View style={{ flex: 1, backgroundColor: 'red' }} >
+        <Text>{data.question}</Text>
+      </View>
       <Pressable style={{ flex: 3, backgroundColor: 'darkorange' }}
-        onPress={() => alert('You pressed orange')}  >
-        <Text>Choice 1</Text>
+        onPress={() => sendMessage('choice1')}  >
+        <Text>{data.choice1}</Text>
       </Pressable>
       <Pressable style={{ flex: 3, backgroundColor: 'green' }} 
-        onPress={() => alert('You pressed green')}  >
-        <Text>Choice 2</Text>
+        onPress={() => sendMessage('choice2')}  >
+        <Text>{data.choice2}</Text>
       </Pressable>
     </View>
   );
@@ -30,3 +57,6 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 });
+
+
+export default App;
--- backend/backend.py
+++ backend/backend.py
@@ -1,13 +1,22 @@
 import asyncio
+import json
 import websockets
 
+test_message = {
+    "choice1": "Choice 1 from server",
+    "choice2": "Choice 2 from server",
+    "question": "Question from server",
+}
+
+
 async def echo(websocket, path):
+    print('sending data to ' + str(websocket))
+    await websocket.send(json.dumps(test_message))
     async for message in websocket:
         print(f'{path}: {message}')
-        await websocket.send(message)
 
 async def main():
-    async with websockets.serve(echo, "localhost", 8080):
+    async with websockets.serve(echo, "0.0.0.0", 8080):
         await asyncio.Future()  # run forever
 
 asyncio.run(main())
