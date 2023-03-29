1867a9035437c04a3a646be086dc031181db4235
Paul Buser <buser.paul@gmail.com>
2023-03-29 19:13:12 -0400

    Random name generation works.

--- audience-app/App.js
+++ audience-app/App.js
@@ -1,50 +1,43 @@
 import { useState, useRef, useEffect } from 'react';
 import { StyleSheet, View, Text, Pressable } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import UserIdModal from "./UserIdModal"
+import { uniqueNamesGenerator, adjectives, colors, animals } from 'unique-names-generator';
 
 
 const WS_BACKEND = "wss://voting-socket.rumpus.club";
 
 const App = () => {
 
-  const [data, setData] = useState({
+  const [ballot, setBallot] = useState({
     choice1: "Choice 1",
     choice2: "Choice 2",
     question: "Question",
   });
 
-  const [userId, setUserId] = useState("");
+  const [votes, setVotes] = useState({
+    choice1: 0,
+    choice2: 0,
+  });
 
-  const [isModalVisible, setModalVisible] = useState(true);
-
-//   const storeData = async (key, value) => {
-//     try {
-//       await AsyncStorage.setItem(key, value)
-//     } catch (e) {
-//       // saving error
-//     }
-//   }
-
-//   const getData = async (key) => {
-//   try {
-//     const value = await AsyncStorage.getItem(key)
-//     if(value !== null) {
-//       // key found
-//       return value;
-//     }
-//   } catch(e) {
-//     // error reading value
-//     console.log(e);
-//   }
-// }
-  // Set up storage of name
-  // useEffect(() => {
-  //   storeData('userId', 'mr. garbage');
-  // }, []);
-
+  const [userId, setUserId] = useState("userId");
 
   const ws = useRef(null);
+
+//  Set up unique name
+  useEffect(() => {
+    AsyncStorage.getItem('userId').then((maybeUserId) => {
+      if (maybeUserId !== null) {
+        setUserId(maybeUserId);
+      } else { // No userId exists
+        const uniqueUserId = uniqueNamesGenerator({
+          dictionaries: [adjectives, colors, animals],
+        });
+        AsyncStorage.setItem('userId', uniqueUserId).then(() => {
+          setUserId(uniqueUserId);
+        });
+      }
+    });
+  }, []);
 
   // Set up WebSocket
   useEffect(() => {
@@ -52,9 +45,9 @@ const App = () => {
     ws.current.onmessage = (message) => {
       console.log(message);
       const newData = JSON.parse(message.data);
-      const oldData = {...data};
+      const oldData = {...ballot};
       const merged = Object.assign(oldData, newData);
-      setData(merged);
+      setBallot(merged);
     };
     return () => ws.current.close();
   }, []);
@@ -76,17 +69,16 @@ const App = () => {
       style={styles.container}>
       <View style={{ flex: 1, backgroundColor: 'red' }} >
         <Text>{userId}</Text>
-        <Text style={[styles.big, styles.question]}>{data.question}</Text>
+        <Text style={[styles.big, styles.question]}>{ballot.question}</Text>
       </View>
       <Pressable style={{ flex: 3, backgroundColor: 'darkorange' }}
         onPress={() => sendMessage('choice1')}  >
-        <Text style={[styles.big, styles.choice]}>{data.choice1}</Text>
+        <Text style={[styles.big, styles.choice]}>{ballot.choice1}</Text>
       </Pressable>
       <Pressable style={{ flex: 3, backgroundColor: 'green' }} 
         onPress={() => sendMessage('choice2')}  >
-        <Text style={[styles.big, styles.choice]}>{data.choice2}</Text>
+        <Text style={[styles.big, styles.choice]}>{ballot.choice2}</Text>
       </Pressable>
-      <UserIdModal isVisible={isModalVisible} onClose={modalCallback} />
     </View>
   );
 };
