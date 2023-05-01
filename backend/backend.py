515e0ad5d7bc40f3df307300a3b8af1f8a56ef10
Paul Buser <buser.paul@gmail.com>
2023-05-01 13:14:44 -0400

    Remove redundant logging.INFO level set

--- backend/backend.py
+++ backend/backend.py
@@ -20,7 +20,6 @@ NAMED_CLIENT_TIMEOUT = timedelta(minutes=5)
 def setup_logging():
     logging.basicConfig(filename=datetime.now().strftime('logs/%Y-%m-%d-%H:%M:%S.log'), level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
     console = logging.StreamHandler()
-    console.setLevel(logging.INFO)
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
     console.setFormatter(formatter)
     logging.getLogger('').addHandler(console)
