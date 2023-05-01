d1cab6c8afc20ed6f559f3ba492fcfaba77f3adb
root <buser.paul@gmail.com>
2023-05-01 20:39:36 +0100

    make backend run on port 443

--- backend/run_backend.sh
+++ backend/run_backend.sh
@@ -2,4 +2,4 @@
 # Note: this script uses `entr` to detect changes to the filesystem
 source /root/live-voting/backend/venv/bin/activate
 # Restart the backend if a python source file changes
-ls /root/live-voting/backend/*.py | entr -r python /root/live-voting/backend/backend.py run_secure /etc/letsencrypt/live/voting-socket.rumpus.club/fullchain.pem /etc/letsencrypt/live/voting-socket.rumpus.club/privkey.pem
+ls /root/live-voting/backend/*.py | entr -r python /root/live-voting/backend/backend.py run_secure /etc/letsencrypt/live/voting-socket.rumpus.club/fullchain.pem /etc/letsencrypt/live/voting-socket.rumpus.club/privkey.pem --port 443
