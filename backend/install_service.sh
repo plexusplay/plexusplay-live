1d0acd068e96f4d87c40f2ca730c3b3283628861
root <buser.paul@gmail.com>
2023-05-01 20:37:55 +0100

    Add systemd unit and related scripts for websocket backend

---
+++ backend/install_service.sh
@@ -0,0 +1,4 @@
+sudo cp live-voting.service /etc/systemd/system/live-voting.service
+sudo chmod 664 /etc/systemd/system/live-voting.service
+sudo systemctl daemon-reload
+sudo systemctl enable --now live-voting.service
--- backend/run_backend.sh
+++ backend/run_backend.sh
@@ -1,2 +1,5 @@
+#!/bin/bash
+# Note: this script uses `entr` to detect changes to the filesystem
 source /root/live-voting/backend/venv/bin/activate
-python /root/live-voting/backend/backend.py run_secure /etc/letsencrypt/live/voting-socket.rumpus.club/fullchain.pem /etc/letsencrypt/live/voting-socket.rumpus.club/privkey.pem
+# Restart the backend if a python source file changes
+ls /root/live-voting/backend/*.py | entr -r python /root/live-voting/backend/backend.py run_secure /etc/letsencrypt/live/voting-socket.rumpus.club/fullchain.pem /etc/letsencrypt/live/voting-socket.rumpus.club/privkey.pem
