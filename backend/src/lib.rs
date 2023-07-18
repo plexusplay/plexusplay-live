cb64a326532b10df77f807e9568c13faf3dbe2e2
Paul Buser <buser.paul@gmail.com>
2023-07-17 22:53:25 -0400

    Multiple votes, multiple threads

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,15 +1,55 @@
-use serde::{Deserialize, Serialize};
 use std::error::Error;
 
-#[derive(Serialize, Deserialize)]
-pub struct Message {
-    pub code: String,
-    pub data: serde_json::Value,
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(tag = "code")]
+pub enum ClientMessage {
+    #[serde(rename = "setBallot")]
+    ClientSetBallot {
+        #[serde(rename = "userId")]
+        user_id: String,
+        data: ClientSetBallotData,
+    },
+    #[serde(rename = "vote")]
+    ClientVote {
+        #[serde(rename = "userId")]
+        user_id: String,
+        data: usize,
+    },
 }
 
-impl Message {
-    pub fn parse(json_str: &str) -> Result<Message, Box<dyn Error>> {
-        let message: Message = serde_json::from_str(json_str)?;
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ClientSetBallotData {
+    pub choices: Vec<String>,
+    pub question: String,
+    pub duration: time::Duration,
+}
+
+impl ClientMessage {
+    pub fn parse(json_str: &str) -> Result<ClientMessage, Box<dyn Error>> {
+        let message: ClientMessage = serde_json::from_str(json_str)?;
         Ok(message)
     }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(tag = "code")]
+pub enum ServerMessage {
+    #[serde(rename = "setBallot")]
+    ServerSetBallot {
+        data: ServerSetBallotData,
+    },
+    #[serde(rename = "setVotes")]
+    ServerSetVotes {
+        data: Vec<u32>,
+    },
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ServerSetBallotData {
+    pub choices: Vec<String>,
+    pub question: String,
+    pub duration: time::Duration,
+    pub expires: time::OffsetDateTime,
 }
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,22 +1,3 @@
-//! A chat server that broadcasts a message to all connections.
-//!
-//! This is a simple line-based server which accepts WebSocket connections,
-//! reads lines from those connections, and broadcasts the lines to all other
-//! connected clients.
-//!
-//! You can test this out by running:
-//!
-//!     cargo run --features="async-std-runtime" --example server 127.0.0.1:12345
-//!
-//! And then in another window run:
-//!
-//!     cargo run --features="async-std-runtime" --example client ws://127.0.0.1:12345/
-//!
-//! You can run the second command in multiple windows and then chat between the
-//! two, seeing the messages from the other client as they're received. For all
-//! connected clients they'll all join the same room and see everyone else's
-//! messages.
-
 use std::{
     collections::HashMap,
     env,
@@ -25,75 +6,93 @@ use std::{
     sync::{Arc, Mutex},
 };
 
-use futures::prelude::*;
-use futures::{
-    channel::mpsc,
-    future, pin_mut,
-};
+use backend::ClientMessage;
+use log::{debug, error, log_enabled, info, Level};
 
-use async_std::net::{TcpListener, TcpStream};
-use async_std::task;
-use async_tungstenite::tungstenite::protocol::Message;
+use futures_channel::mpsc::{unbounded, UnboundedSender};
+use futures_util::{future, pin_mut, stream::TryStreamExt, StreamExt};
 
-type Tx = mpsc::UnboundedSender<Message>;
+use tokio::{net::{TcpListener, TcpStream}, spawn};
+use tokio_tungstenite::tungstenite::protocol::Message;
+
+type Tx = UnboundedSender<Message>;
 type PeerMap = Arc<Mutex<HashMap<SocketAddr, Tx>>>;
+type Votes = Arc<Mutex<Vec<i32>>>;
 
-async fn handle_connection(peer_map: PeerMap, raw_stream: TcpStream, addr: SocketAddr) {
-    println!("Incoming TCP connection from: {}", addr);
+const VOTE_SIZE: usize = 4;
 
-    let ws_stream = async_tungstenite::accept_async(raw_stream)
+async fn handle_connection(peer_map: PeerMap, raw_stream: TcpStream, addr: SocketAddr, votes: Votes) {
+    info!("Incoming TCP connection from: {}", addr);
+
+    let ws_stream = tokio_tungstenite::accept_async(raw_stream)
         .await
         .expect("Error during the websocket handshake occurred");
-    println!("WebSocket connection established: {}", addr);
+    info!("WebSocket connection established: {}", addr);
 
     // Insert the write part of this peer to the peer map.
-    let (tx, rx) = mpsc::unbounded();
+    let (tx, rx) = unbounded();
     peer_map.lock().unwrap().insert(addr, tx);
 
     let (outgoing, incoming) = ws_stream.split();
 
-    let broadcast_incoming = incoming
-        .try_filter(|msg| {
-            // Broadcasting a Close message from one client
-            // will close the other clients.
-            future::ready(!msg.is_close())
-        })
-        .try_for_each(|msg| {
-            println!(
-                "Received a message from {}: {}",
-                addr,
-                msg.to_text().unwrap()
-            );
-            let peers = peer_map.lock().unwrap();
+    let process_incoming = incoming.try_for_each(|ws_msg| {
+        debug!("Received a message from {}: {}", addr, ws_msg.to_text().unwrap());
+        // let peers = peer_map.lock().unwrap();
 
-            // We want to broadcast the message to everyone except ourselves.
-            let broadcast_recipients = peers
-                .iter()
-                .filter(|(peer_addr, _)| peer_addr != &&addr)
-                .map(|(_, ws_sink)| ws_sink);
+        // // We want to broadcast the message to everyone except ourselves.
+        // let broadcast_recipients =
+        //     peers.iter().filter(|(peer_addr, _)| peer_addr != &&addr).map(|(_, ws_sink)| ws_sink);
 
-            for recp in broadcast_recipients {
-                recp.unbounded_send(msg.clone()).unwrap();
+        // for recp in broadcast_recipients {
+        //     recp.unbounded_send(msg.clone()).unwrap();
+        // }
+
+        let parsed = match ws_msg {
+            Message::Text(str) => ClientMessage::parse(&str),
+            _ => return future::ok(()),
+        };
+
+        let msg = match parsed {
+            Ok(msg) => msg,
+            Err(e) => {
+                debug!("{}", e);
+                return future::ok(());
+            },
+        };
+
+        match msg {
+            ClientMessage::ClientSetBallot { user_id, data } => todo!(),
+            ClientMessage::ClientVote { user_id, data } => {
+                if (data >= VOTE_SIZE) {
+                    debug!("vote index >= than VOTE_SIZE: {}", data);
+                    return future::ok(());
+                }
+                votes.lock().unwrap()[data] += 1;
+                println!("{:?}", votes.lock().unwrap());
             }
+        }
 
-            future::ok(())
-        });
 
-    let receive_from_others = rx.map(Ok).forward(outgoing);
+        future::ok(())
+    });
 
-    pin_mut!(broadcast_incoming, receive_from_others);
-    future::select(broadcast_incoming, receive_from_others).await;
+    process_incoming.await;
 
-    println!("{} disconnected", &addr);
+
+    info!("{} disconnected", &addr);
     peer_map.lock().unwrap().remove(&addr);
 }
 
-async fn run() -> Result<(), IoError> {
-    let addr = env::args()
-        .nth(1)
-        .unwrap_or_else(|| "127.0.0.1:8080".to_string());
+#[tokio::main]
+async fn main() -> Result<(), IoError> {
+
+    env_logger::init();
+
+    let addr = env::args().nth(1).unwrap_or_else(|| "127.0.0.1:8080".to_string());
 
     let state = PeerMap::new(Mutex::new(HashMap::new()));
+    let votes = vec![0; VOTE_SIZE];
+    let votes = Arc::new(Mutex::new(votes));
 
     // Create the event loop and TCP listener we'll accept connections on.
     let try_socket = TcpListener::bind(&addr).await;
@@ -102,12 +101,8 @@ async fn run() -> Result<(), IoError> {
 
     // Let's spawn the handling of each connection in a separate task.
     while let Ok((stream, addr)) = listener.accept().await {
-        task::spawn(handle_connection(state.clone(), stream, addr));
+        tokio::spawn(handle_connection(state.clone(), stream, addr, votes.clone()));
     }
 
     Ok(())
-}
-
-fn main() -> Result<(), IoError> {
-    task::block_on(run())
 }
