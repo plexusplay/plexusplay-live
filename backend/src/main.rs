32a0a32492804ddbc2238a768d72f255f8cddfdd
Paul Buser <buser.paul@gmail.com>
2023-08-19 16:46:07 -0400

    add admin functionality

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -9,7 +9,6 @@ use std::sync::{
 use futures::join;
 use futures_util::{SinkExt, StreamExt, TryFutureExt};
 use log::debug;
-use tokio::sync::mpsc::UnboundedSender;
 use tokio::sync::{mpsc, RwLock};
 use tokio_stream::wrappers::UnboundedReceiverStream;
 use warp::ws::{Message, WebSocket};
@@ -23,11 +22,11 @@ const BALLOT_SIZE: usize = 4;
 /// Our global unique user id counter.
 static NEXT_USER_ID: AtomicUsize = AtomicUsize::new(1);
 
-/// Our state of currently connected users.
-///
-/// - Key is their id
-/// - Value is a sender of `warp::ws::Message`
-type Users = RwLock<HashMap<usize, mpsc::UnboundedSender<Message>>>;
+struct User {
+    admin: bool,
+    channel: mpsc::UnboundedSender<Message>,
+}
+type Users = RwLock<HashMap<usize, User>>;
 type VoteMap = RwLock<HashMap<usize, usize>>;
 type Ballot = RwLock<ServerSetBallotData>;
 
@@ -42,8 +41,6 @@ struct StateData{
 async fn main() {
     env_logger::init();
 
-    // Keep track of all connected users, key is usize, value
-    // is a websocket sender.
     let ballot_data = ServerSetBallotData {
         choices: vec![String::from("Init choice"); BALLOT_SIZE],
         question: String::from("Question from server"),
@@ -54,25 +51,39 @@ async fn main() {
         users: Users::default(),
         votes: VoteMap::default(),
         ballot: RwLock::new(ballot_data),
-    });    // Turn our "state" into a new Filter...
-    let state2 = state.clone();
-    let users = warp::any().map(move || state2.clone());
+    });
+    // Turn our "state" into a new Filter...
+    let state_ref = state.clone();
+    let state_ref2 = state.clone();
+    let state_filter = warp::any().map(move || state_ref.clone());
+    let state_filter_admin = warp::any().map(move || state_ref2.clone());
 
     let ws_endpoint = warp::path::end()
         // The `ws()` filter will prepare Websocket handshake...
         .and(warp::ws())
-        .and(users)
+        .and(state_filter)
         .map(|ws: warp::ws::Ws, users| {
             // This will call our function if the handshake succeeds.
-            ws.on_upgrade(move |socket| user_connected(socket, users))
+            ws.on_upgrade(move |socket| user_connected(socket, users, false))
         });
 
-    let vote_send_task = tokio::task::spawn(async move {
+    let admin_endpoint = warp::path("admin")
+        // The `ws()` filter will prepare Websocket handshake...
+        .and(warp::ws())
+        .and(state_filter_admin)
+        .map(|ws: warp::ws::Ws, users| {
+            // This will call our function if the handshake succeeds.
+            ws.on_upgrade(move |socket| user_connected(socket, users, true))
+        });
+
+    let state = state.clone();
+
+    let polling_data_task = tokio::task::spawn(async move {
         let mut interval = tokio::time::interval(tokio::time::Duration::from_millis(300));
 
         loop {
             interval.tick().await;
-            send_votes_to_all(state.clone()).await;
+            send_polling_data(state.clone()).await;
         }
     });
 
@@ -85,10 +96,11 @@ async fn main() {
         .expect("Unable to parse socket address");
 
     println!("running on {}", addr);
-    let _ = join!(warp::serve(ws_endpoint).run(addr), vote_send_task);
+    let routes = ws_endpoint.or(admin_endpoint);
+    let _ = join!(warp::serve(routes).run(addr), polling_data_task);
 }
 
-async fn user_connected(ws: WebSocket, state: State) {
+async fn user_connected(ws: WebSocket, state: State, admin: bool) {
     // Use a counter to assign a new unique ID for this user.
     let my_id = NEXT_USER_ID.fetch_add(1, Ordering::Relaxed);
 
@@ -101,6 +113,10 @@ async fn user_connected(ws: WebSocket, state: State) {
     // to the websocket...
     let (tx, rx) = mpsc::unbounded_channel();
     let mut rx = UnboundedReceiverStream::new(rx);
+    let user = User {
+        admin: admin,
+        channel: tx.clone(),
+    };
 
     tokio::task::spawn(async move {
         while let Some(message) = rx.next().await {
@@ -114,7 +130,13 @@ async fn user_connected(ws: WebSocket, state: State) {
     });
 
 
-    greet_user(my_id, tx, state.clone()).await;
+    // Save the sender in our list of connected users.
+    state.users.write().await.insert(my_id, user);
+    // Send the user the current ballot.
+    let ballot = state.ballot.read().await.serialize();
+    if let Err(_err) = tx.send(Message::text(ballot)) {
+        eprint!("couldn't send ballot to {}", my_id);
+    }
 
 
     // Return a `Future` that is basically a state machine managing
@@ -136,16 +158,6 @@ async fn user_connected(ws: WebSocket, state: State) {
     // user_ws_rx stream will keep processing as long as the user stays
     // connected. Once they disconnect, then...
     user_disconnected(my_id, &state).await;
-}
-
-async fn greet_user(my_id: usize, tx: UnboundedSender<Message>, state: State) {
-    // Save the sender in our list of connected users.
-    state.users.write().await.insert(my_id, tx.clone());
-    // Send the user the current ballot.
-    let ballot = state.ballot.read().await.serialize();
-    if let Err(_err) = tx.send(Message::text(ballot)) {
-        eprint!("couldn't send ballot to {}", my_id);
-    }
 }
 
 async fn user_message(my_id: usize, msg: Message, state: &State) {
@@ -187,19 +199,42 @@ async fn user_message(my_id: usize, msg: Message, state: &State) {
     // New message from this user, send it to everyone else (except same uid)...
 }
 
-async fn send_votes_to_all(state: State) {
+async fn send_polling_data(state: State) {
     let votes = collate_votes(state.clone()).await;
-    send_to_all(votes.to_string(), state).await;
+    send_to_all(votes.to_string(), state.clone()).await;
+    let metadata = metadata(state.clone()).await;
+    send_to_admins(metadata.to_string(), state.clone()).await;
+}
+
+async fn send_to_admins(msg: String, state: State) {
+    _send_to_all(msg, state.clone(), true).await;
 }
 
 async fn send_to_all(msg: String, state: State) {
-    for (_, tx) in state.users.read().await.iter() {
-        if let Err(_disconnected) = tx.send(Message::text(&msg)) {
+    _send_to_all(msg, state.clone(), false).await;
+}
+
+async fn _send_to_all(msg: String, state: State, admin_only: bool) {
+    for (_, user) in state.users.read().await.iter() {
+        if admin_only && (!user.admin) {
+            return;
+        }
+        if let Err(_disconnected) = user.channel.send(Message::text(&msg)) {
             // The tx is disconnected, our `user_disconnected` code
             // should be happening in another task, nothing more to
             // do here.
         }
     }
+}
+
+async fn metadata(state: State) -> serde_json::Value {
+    let users = state.users.read().await.len();
+    return serde_json::json!({
+        "code": "metadata",
+        "data": {
+            "users": users,
+        },
+    });
 }
 
 async fn collate_votes(state: State) -> serde_json::Value {
